9221b6f7d4926517a80155b9071035b4b6beafab
Moeez Ahmed <moeez.ahmad@outlook.com>
2025-05-04 04:27:50 +0500

    feat: add About and Home components with routing, and implement basic styling
    - Created About component with a hero image and content
    - Created Home component with a call to action
    - Set up routing in App component using React Router
    - Added global styles and specific styles for Home and About pages
    - Included SVG asset for React logo
    - Configured Vite for project setup

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router';
+import Home from './Home';
+import About from './About';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <header>
+      <Link className='site-logo' to="/">#VANLIFE</Link>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from "react-dom/client"
+import App from "./components/App"
+import './index.css'
+
+React.createRoot(document.getElementById("root"))
+  .render(<App />);
