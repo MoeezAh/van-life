691d4a72f41c736cd099b2e8358fddb1284a8c9c
Moeez Ahmed <moeez.ahmad@outlook.com>
2025-05-06 00:01:10 +0500

    feat: implement Vans and VanDetail components with routing and styling, and set up MirageJS for mock API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router';
 import Home from './Home';
 import About from './About';
+import Vans from './Vans.jsx';
+import VanDetail from './VanDetail.jsx';
+
+import "../server.js";
 
 export default function App() {
   return (
@@ -9,11 +13,14 @@ export default function App() {
       <Link className='site-logo' to="/">#VANLIFE</Link>
       <nav>
         <Link to="/about">About</Link>
+        <Link to="/vans">Vans</Link>
       </nav>
     </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
