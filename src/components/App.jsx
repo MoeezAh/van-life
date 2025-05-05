316b16858d2e165ec470bacb10bf286ba55829e9
Moeez Ahmed <moeez.ahmad@outlook.com>
2025-05-06 00:36:02 +0500

    feat: add Layout and Header components for improved routing structure and navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,22 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router';
 import Home from './Home';
 import About from './About';
-import Vans from './Vans.jsx';
-import VanDetail from './VanDetail.jsx';
+import Vans from './Vans';
+import VanDetail from './VanDetail';
+import Layout from "./Layout"
 
-import "../server.js";
+import '../server.js';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <header>
-      <Link className='site-logo' to="/">#VANLIFE</Link>
-      <nav>
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
-      </nav>
-    </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
